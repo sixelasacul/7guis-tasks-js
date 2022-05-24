8f39aef908b054408e109ed766a09230eb6da1f9
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2022-05-24 01:06:00 +0100

    Small update - Fixed linting - Added README

---
+++ README.md
@@ -0,0 +1,13 @@
+# 7guis-tasks-js
+
+My very own, very not perfect, very intuitive attempt to implement the 7 GUIs tasks in plain JS (no other dependency).
+
+Useful links: [7GUIs reference](https://eugenkiss.github.io/7guis/tasks) | [HackerNews source](https://news.ycombinator.com/item?id=31273802)
+
+Other implementations: [React JS](https://codesandbox.io/s/7guis-tasks-react-dxj2yd) | [Solid JS](https://codesandbox.io/s/7guis-tasks-solid-j99utk)
+
+## TODOs
+
+- [ ] Add some styling
+- [ ] Publish websites
+- [ ] Add explanations on why I implemented this way
--- src/crud.js
+++ src/crud.js
@@ -1,94 +1,90 @@
 function addUser(selectNode, name, surname) {
-  const lastOption = [...selectNode.querySelectorAll("option")].at(-1);
-  const option = document.createElement("option");
-  option.textContent = `${surname}, ${name}`;
-  option.value = lastOption?.value ? Number(lastOption.value) + 1 : 0;
-  selectNode.appendChild(option);
+  const lastOption = [...selectNode.querySelectorAll('option')].at(-1)
+  const option = document.createElement('option')
+  option.textContent = `${surname}, ${name}`
+  option.value = lastOption?.value ? Number(lastOption.value) + 1 : 0
+  selectNode.appendChild(option)
 }
 function updateUser(selectNode, value, name, surname) {
-  const selectedOption = selectNode.querySelector(`option[value='${value}']`);
-  selectedOption.textContent = `${surname}, ${name}`;
+  const selectedOption = selectNode.querySelector(`option[value='${value}']`)
+  selectedOption.textContent = `${surname}, ${name}`
 }
 function deleteUser(selectNode, value) {
-  const selectedOption = selectNode.querySelector(`option[value='${value}']`);
-  selectNode.removeChild(selectedOption);
+  const selectedOption = selectNode.querySelector(`option[value='${value}']`)
+  selectNode.removeChild(selectedOption)
 }
 
-const usersSelect = document.querySelector("#crud-select");
-const filterInput = document.querySelector("#crud-filter");
-const nameInput = document.querySelector("#crud-name");
-const surnameInput = document.querySelector("#crud-surname");
-const createButton = document.querySelector("#crud-create");
-const updateButton = document.querySelector("#crud-update");
-const deleteButton = document.querySelector("#crud-delete");
+const usersSelect = document.querySelector('#crud-select')
+const filterInput = document.querySelector('#crud-filter')
+const nameInput = document.querySelector('#crud-name')
+const surnameInput = document.querySelector('#crud-surname')
+const createButton = document.querySelector('#crud-create')
+const updateButton = document.querySelector('#crud-update')
+const deleteButton = document.querySelector('#crud-delete')
 
 function setCreateUpdateState(isNameSet, isSurnameSet, isUserSelected) {
   if (isNameSet && isSurnameSet) {
-    createButton.removeAttribute("disabled");
+    createButton.removeAttribute('disabled')
     if (isUserSelected) {
-      updateButton.removeAttribute("disabled");
+      updateButton.removeAttribute('disabled')
     }
-    return;
+    return
   }
-  updateButton.setAttribute("disabled", "true");
-  createButton.setAttribute("disabled", "true");
+  updateButton.setAttribute('disabled', 'true')
+  createButton.setAttribute('disabled', 'true')
 }
 
-nameInput.addEventListener("change", (e) => {
+nameInput.addEventListener('change', (e) => {
   setCreateUpdateState(
     !!e.target.value,
     !!surnameInput.value,
     !!usersSelect.value
-  );
-});
+  )
+})
 
-surnameInput.addEventListener("change", (e) => {
-  setCreateUpdateState(
-    !!nameInput.value,
-    !!e.target.value,
-    !!usersSelect.value
-  );
-});
+surnameInput.addEventListener('change', (e) => {
+  setCreateUpdateState(!!nameInput.value, !!e.target.value, !!usersSelect.value)
+})
 
-filterInput.addEventListener("change", (e) => {
-  const options = usersSelect.querySelectorAll("option");
+filterInput.addEventListener('change', (e) => {
+  const options = usersSelect.querySelectorAll('option')
   options.forEach((option) => {
     if (option.textContent.startsWith(e.target.value)) {
-      option.removeAttribute("hidden");
+      option.removeAttribute('hidden')
     } else {
-      option.setAttribute("hidden", "true");
+      option.setAttribute('hidden', 'true')
     }
-  });
-});
+  })
+})
 
-usersSelect.addEventListener("change", (e) => {
-  const value = e.target.value;
+usersSelect.addEventListener('change', (e) => {
+  const value = e.target.value
   if (value !== null && value !== undefined) {
-    deleteButton.removeAttribute("disabled");
+    deleteButton.removeAttribute('disabled')
     if (!!nameInput.value && !!surnameInput.value) {
-      updateButton.removeAttribute("disabled");
+      updateButton.removeAttribute('disabled')
     } else {
-      updateButton.setAttribute("disabled", "true");
+      updateButton.setAttribute('disabled', 'true')
     }
   } else {
-    deleteButton.setAttribute("disabled", "true");
+    deleteButton.setAttribute('disabled', 'true')
   }
-});
+})
 
-createButton.addEventListener("click", () => {
-  addUser(usersSelect, nameInput.value, surnameInput.value);
-});
+createButton.addEventListener('click', () => {
+  addUser(usersSelect, nameInput.value, surnameInput.value)
+})
 
-updateButton.addEventListener("click", () => {
+updateButton.addEventListener('click', () => {
   updateUser(
     usersSelect,
     usersSelect.value,
     nameInput.value,
     surnameInput.value
-  );
-});
+  )
+})
 
-deleteButton.addEventListener("click", () => {
-  deleteUser(usersSelect, usersSelect.value);
-  deleteButton.setAttribute("disabled", "true");
-});
+deleteButton.addEventListener('click', () => {
+  deleteUser(usersSelect, usersSelect.value)
+  deleteButton.setAttribute('disabled', 'true')
+})
